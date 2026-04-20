790e10fab4a0f223bb2f62436bb72cd787f5868b
rosariomary754-design <rosariomary754@gmail.com>
2026-04-20 14:49:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const SUPABASE_URL = "https://nfcpbzwfufbotrlytden.supabase.co"
 const SUPABASE_KEY = "sb_publishable_E5GQnvQYduf2ODhxl1Cigw_J1w_57EN"
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // Mock Data for Testing
 const beats = [
